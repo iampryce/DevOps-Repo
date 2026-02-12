a5a3522a4dbc7a0a91da315b430f9d052dab0b00
Tobi <thesuperjocks@gmail.com>
2026-02-12 11:09:55 +0000

    temp console changes

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Changed again to - Shutting down gracefully...');
+  console.log('Changed again to - Shutting down successfully...');
   await mongoose.connection.close();
   process.exit(0);
 });
